3c2b70d480ba563876ff6cecb4d89653b4adb4d6
Erik Shandrowski <erik843@gmail.com>
2014-06-30 14:56:13 -0600

    Done through 6

--- function-practice-2.js
+++ function-practice-2.js
@@ -21,3 +21,37 @@ var totalLetters = function(strArray) {
 totalLetters(longString);
 
 // 3
+var keyValue = function(key, value) {
+	var wantedValue = {};
+	wantedValue[key] = value;
+	return wantedValue;
+}
+keyValue('city', 'Denver');
+
+//4
+var testArray1 = ['a', 'b', 'c', 'd','e'];
+var testArray2 = ['Erik', 'Stasia', 'Marne', 'Dennis'];
+var negativeIndex = function(enteredArray, negativeNumber) {
+	return enteredArray[enteredArray.length + negativeNumber];
+}
+
+// 5
+var removeM = function(word) {
+	var newWord = ''
+	for(i=0; i < word.length; i++) {
+		if (word.charAt(i) !== 'm') {
+			newWord += word.charAt(i);
+		}
+		else if (word.charAt(i) === 'm') {
+			newWord += ''
+		}
+	}
+	return newWord;
+
+
+//6
+var printObject = function(insertedObject) {
+	for(var key in insertedObject) {
+		console.log(key + " is " + insertedObject[key]);
+	}
+}
